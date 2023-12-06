1a88d5d27ec36bb15eb8cf2dc824bb3a7ed76880
richa <aliado.richa@gmail.com>
2023-12-07 00:39:51 +0800

    added a simple function for the Click Here button

--- app/src/main/java/com/aliado/richa/block1/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/aliado/richa/block1/p1/quiz/MainActivity.kt
@@ -2,10 +2,32 @@ package com.aliado.richa.block1.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.aliado.richa.block1.p1.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btClickHere.setOnClickListener {
+            showPopUp()
+        }
+    }
+    private fun showPopUp() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("ITE393 P1 Quiz")
+            .setMessage("Merry Christmas!")
+            .setPositiveButton("Likewise") {dialog, which ->
+                Toast.makeText(this, "ho ho ho", Toast.LENGTH_SHORT).show()
+            }
+            .setNegativeButton("Okay") {dialog, which ->
+                dialog?.dismiss()
+            }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
     }
 }
